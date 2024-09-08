d4b7f21166366136d4d3b1fd95a8d4feda5dd0f8
ash-wing <ayesheesinha91@gmail.com>
2024-09-08 10:37:03 +0530

    Project wanderlust completed part 3c: implemented search button icons on navbar frontend

--- app.js
+++ app.js
@@ -57,9 +57,9 @@ const sessionOptions = {
         httpOnly: true,
     }
 };
-app.get("/", (req, res) => {
-    res.send("Hi! I am root ");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hi! I am root ");
+// });
 
 
 app.use(session(sessionOptions));
