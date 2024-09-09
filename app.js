911fb3ffc93cb4147e0d9de448942340bd0f1086
ash-wing <ayesheesinha91@gmail.com>
2024-09-09 08:05:23 +0530

    added engine node version for deployment in render

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const methodOverride = require ("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -24,7 +25,10 @@ const userRouter = require("./routes/user.js");
 
 
 
-const mongourl = "mongodb://127.0.0.1:27017/wanderlust"; //creating the database
+// const mongourl = "mongodb://127.0.0.1:27017/wanderlust"; //creating the database
+
+const dbUrl = process.env.ATLASDB_URL
+
 
 // Calling the main function
 main().then(() => {
@@ -34,7 +38,7 @@ main().then(() => {
 });
 
 async function main() {      //this is the main function
-    await mongoose.connect(mongourl);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -47,7 +51,20 @@ app.engine ('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"public")))
 
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret:"mysupersecretcode",
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", ()=>{
+    console.log("ERROR in MONGO SESSION STORE", err);
+})
+
 const sessionOptions = {
+    store,
     secret : "mysupersecretcode",
     resave:false,
     saveUninitialized : true,
@@ -60,6 +77,8 @@ const sessionOptions = {
 // app.get("/", (req, res) => {
 //     res.send("Hi! I am root ");
 // });
+
+
 
 
 app.use(session(sessionOptions));
